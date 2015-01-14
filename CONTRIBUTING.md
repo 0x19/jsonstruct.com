21afb3b75ce8709aef19870213e8339dc1a20f75
0x19 <nevio.vesic@gmail.com>
2015-01-14 21:48:05 +0100

    Yet another round of initial stuff

---
+++ CONTRIBUTING.md
@@ -0,0 +1,5 @@
+# Contributing
+
+So you wish to contribute to JSONStruct? Well that's simple. Just a.) reach me over my email or b.) submit patch
+
+Nothing else to add to contribution at this moment.
---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'bootstrap-sass'
+
+http_path = "/public"
+css_dir = "/public/stylesheets"
+sass_dir = "/scss"
+images_dir = "/public/images"
+javascripts_dir = "/public/components"
+
+output_style = :compressed
